e2eba3e2c3ff1320e641134900292f8851b7cfa0
Light Distribution Channel <distributionchannelforscience@gmail.com>
2026-07-29 07:33:57 +0000

    website with contant

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html", title="Community Good News")
+    return render_template("index.html", title="Holy Harvest Charity Ministries")
